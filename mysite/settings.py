8f4a0468b66001b8df6c2c2aa01642b3e26c117f
dallyura <dallyura@gmail.com>
2020-12-14 16:50:18 +0800

    add requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+# import dj_database_url
 #
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -71,6 +73,8 @@ DATABASES = {
     }
 }
 
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
